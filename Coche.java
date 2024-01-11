cc6db134d8a30603a91f0390cc9672be08af525c
mcas2 <mcas2@email.com>
2024-01-11 09:50:02 +0100

    Clase coche

---
+++ Coche.java
@@ -0,0 +1,39 @@
+public class Coche {
+    public String marca;
+    public long kilometros;
+    public String modelo;
+    public int caballos;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public long getKilometros() {
+        return kilometros;
+    }
+
+    public void setKilometros(long kilometros) {
+        this.kilometros = kilometros;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCaballos() {
+        return caballos;
+    }
+
+    public void setCaballos(int caballos) {
+        this.caballos = caballos;
+    }
+}
+
